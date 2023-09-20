aca8b528a1609aaec4d2f9bd417ccdfbe04bbfc1
DerJerry <jeromejnca5@hotmail.com>
2023-09-20 17:41:45 +0200

    Funktionenaufteilung

--- app.py
+++ app.py
@@ -1,31 +1,12 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, request, redirect, url_for, session, make_response
 import random
 import mysql.connector
 import pygame
 
-# Initialisieren von pygame
-pygame.init()
-
-# Hintergrundmusik laden
-background_music = pygame.mixer.Sound('static/Hintergrundmusik.mp3')
-background_music.set_volume(0.3)
-
-# Hintergrundmusik abspielen (Endlosschleife)
-background_music.play(-1)
 
 app = Flask(__name__)
 app.secret_key = "PushinP"
-
-mydb = mysql.connector.connect(
-  host="localhost",
-  user="root",
-  password=""
-)
-
-mycursor = mydb.cursor()
-
-mycursor.execute("CREATE DATABASE IF NOT EXISTS games")
-
+background_music = ""
 # Verbindung zur MySQL-Datenbank herstellen
 db = mysql.connector.connect(
     host="localhost",
@@ -35,19 +16,31 @@ db = mysql.connector.connect(
 )
 cursor = db.cursor()
 
-# Tabelle erstellen, wenn sie nicht existiert
-cursor.execute("""
-    CREATE TABLE IF NOT EXISTS scores (
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        name VARCHAR(255) NOT NULL,
-        attempts INT NOT NULL
-    )
-""")
-db.commit()
+# Funktion zur Initialisierung der Einstellungen
+def load_settings():
+    global db 
+    # Initialisieren von pygame
+    pygame.init()
+
+    # Hintergrundmusik laden und einstellen
+    background_music = pygame.mixer.Sound('static/Hintergrundmusik.mp3')
+    background_music.set_volume(0.3)
+    background_music.play(-1)
+    
+    # Verbindung zur MySQL-Datenbank herstellen, falls sie geschlossen wurde
+    if not db.is_connected():
+        db = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="",
+            database="games"
+        )
+    
 
 # Startseite 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    
     if request.method == 'POST':
         # Zufällige Nummer generieren und in der Session speichern
         session['target_number'] = random.randint(1, 100)
@@ -92,6 +85,7 @@ def game():
 # Scoreboard-Seite
 @app.route('/scoreboard')
 def scoreboard():
+
     cursor.execute("SELECT name, attempts FROM scores ORDER BY attempts")
     scores = cursor.fetchall()
     return render_template('scoreboard.html', scores=scores)
@@ -113,9 +107,9 @@ def save_options():
     response = app.make_response(redirect(url_for('index')))
     response.set_cookie('language', str(language), max_age=365*24*60*60)
     response.set_cookie('font-size', str(font_size), max_age=365*24*60*60)
-    response = app.make_response(render_template('index.html', current_volume=new_volume))
     response.set_cookie('volume', str(new_volume), max_age=365*24*60*60)
     return response
 
 if __name__ == '__main__':
+    load_settings()
     app.run(debug=True)
